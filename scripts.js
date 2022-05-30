07a252f7c97819e6f7f6224a3c6136b1cb95e1d0
Lulytass <88911233+Lulytass@users.noreply.github.com>
2022-05-30 16:11:10 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -76,6 +76,18 @@ function check(e) {
     }
   }
 }
+
+// function checkLink(x) {
+//   for (var i = 0; i < navlinkAll.length; i++) {
+//     navlinkAll[i].addEventListener("click", function () {
+//       if (window.location.href == x.href) {
+//         navMenuDiv.classList.add("hidden");
+//       }
+//     });
+//   }
+// }
+
+
 function checkParent(t, elm) {
   while (t.parentNode) {
     if (t == elm) {
@@ -100,3 +112,73 @@ function activeLink(x) {
   navMenuDiv.classList.add("hidden");
   x.classList.add("nav-link-active");
 }
+
+// navlink.addEventListener("click", function () {
+//   navlink.classList.add("nav-link-active");
+// });
+// function AddClassPrice(x) {
+//   // REMOVE CLASS
+//   x.classList.remove("flex");
+//   x.classList.remove("flex-col");
+//   x.classList.remove("w-5/6");
+//   x.classList.remove("lg:w-1/4");
+//   x.classList.remove("mx-auto");
+//   x.classList.remove("lg:mx-0");
+//   x.classList.remove("rounded-none");
+//   x.classList.remove("lg:rounded-l-lg");
+//   x.classList.remove("bg-white");
+//   x.classList.remove("mt-4");
+//   // ADD CLASS
+//   x.classList.add("flex");
+//   x.classList.add("flex-col");
+//   x.classList.add("w-5/6");
+//   x.classList.add("lg:w-1/3");
+//   x.classList.add("mx-auto");
+//   x.classList.add("lg:mx-0");
+//   x.classList.add("rounded-lg");
+//   x.classList.add("bg-white");
+//   x.classList.add("mt-4");
+//   x.classList.add("sm:-mt-6");
+//   x.classList.add("shadow-lg");
+//   x.classList.add("z-10");
+// }
+
+// function RemoveClassPrice(x) {
+//   // REMOVE CLASS
+//   x.classList.remove("flex");
+//   x.classList.remove("flex-col");
+//   x.classList.remove("w-5/6");
+//   x.classList.remove("lg:w-1/3");
+//   x.classList.remove("mx-auto");
+//   x.classList.remove("lg:mx-0");
+//   x.classList.remove("rounded-lg");
+//   x.classList.remove("bg-white");
+//   x.classList.remove("mt-4");
+//   x.classList.remove("sm:-mt-6");
+//   x.classList.remove("shadow-lg");
+//   x.classList.remove("z-10");
+// // ADD CLASS
+//   x.classList.add("flex");
+//   x.classList.add("flex-col");
+//   x.classList.add("w-5/6");
+//   x.classList.add("lg:w-1/4");
+//   x.classList.add("mx-auto");
+//   x.classList.add("lg:mx-0");
+//   x.classList.add("rounded-none");
+//   x.classList.add("lg:rounded-l-lg");
+//   x.classList.add("bg-white");
+//   x.classList.add("mt-4");
+
+// }
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
