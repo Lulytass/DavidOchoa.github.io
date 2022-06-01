5cb1619594bc9625071274126997dc5f583b5b44
Lulytass <88911233+Lulytass@users.noreply.github.com>
2022-05-31 21:25:15 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -4,49 +4,12 @@ var navcontent = document.getElementById("nav-content");
 var navaction = document.getElementById("navAction");
 var toToggle = document.querySelectorAll(".toggleColour");
 var navlink = document.getElementById("nav-link");
-// var price = document.getElementById("price");
 
-document.addEventListener("scroll", function () {
-  /*aplica las clases al hacer scroll*/
-  scrollpos = window.scrollY;
 
-  if (scrollpos > 10) {
-    header.classList.add("bg-white");
-    navaction.classList.remove("bg-white");
-    navaction.classList.add("gradient");
-    navaction.classList.remove("text-gray-800");
-    navaction.classList.add("text-white");
-    navlink.classList.remove("nav-link-active");
-
-    for (var i = 0; i < toToggle.length; i++) {
-      toToggle[i].classList.add("text-blue-800");
-      toToggle[i].classList.remove("text-white");
-    }
-    header.classList.add("shadow");
-    navcontent.classList.remove("bg-gray-50");
-    navcontent.classList.remove("md:text-white");
-    navcontent.classList.add("bg-white");
-    navcontent.classList.add("text-gray-700");
-  } else {
-    header.classList.remove("bg-white");
-    navaction.classList.remove("gradient");
-    navaction.classList.add("bg-white");
-    navaction.classList.remove("text-white");
-    navaction.classList.add("text-gray-800");
-    navlink.classList.add("nav-link-active");
-
-    for (var i = 0; i < toToggle.length; i++) {
-      toToggle[i].classList.add("text-white");
-      toToggle[i].classList.remove("text-blue-800");
-    }
-
-    header.classList.remove("shadow");
-    navcontent.classList.remove("bg-white");
-    navcontent.classList.add("bg-gray-50");
-    navcontent.classList.add("text-gray-700");
-    navcontent.classList.add("md:text-white");
-  }
-});
+window.addEventListener("scroll", function(){
+  var header = document.querySelector("header");
+  header.classList.toggle("abajo", this.window.scrolly>5);
+})
 
 /*Responsive NAV*/
 var navlinkAll = document.querySelectorAll("#nav-link");
@@ -170,15 +133,3 @@ function activeLink(x) {
 //   x.classList.add("mt-4");
 
 // }
-© 2022 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
